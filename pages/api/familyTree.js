24149cad6bb3b339f867398095f8e367c4ea11cf
Laith M Maali <laith.maali@WW.com>
2025-02-21 10:52:29 +0300

    Add files via upload

--- pages/api/familyTree.js
+++ pages/api/familyTree.js
@@ -1,10 +1,7 @@
 import { supabase } from '../../lib/supabaseClient';
 export default async function handler(req, res) {
-    const { id, name, image, parentid } = req.body;
-    const { data, error } = await supabase
-        .from('family_tree')
-        .update({ name, image, parentid })
-        .eq('id', id);
+    const { data, error } = await supabase.from('family_tree').select('*');
+    console.log(`data: ${data}`);
     if (error) return res.status(500).json({ error: error.message });
     res.status(200).json(data);
 }
